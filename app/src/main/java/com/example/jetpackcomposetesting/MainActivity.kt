3bb2584486e7caead5c08c1f42b975e97ac77d1a
ywpark <ywpark@smartlo.net>
2023-05-22 16:18:51 +0900

    #jetpackComposeTesting Compose, kotlin, compiler 버전 맞춤. compose 초기라 각 버전별 호환성이 맞아야 동작 가능.

--- app/src/main/java/com/example/jetpackcomposetesting/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetesting/MainActivity.kt
@@ -1,43 +1,138 @@
 package com.example.jetpackcomposetesting
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposetesting.ui.theme.JetpackComposeTestingTheme
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TextField
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTestingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Hello()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(group = "my-group-1", device = Devices.AUTOMOTIVE_1024p)
+fun GreetingWrapper() {
+    Greeting("Jetpack Compose")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTestingTheme {
-        Greeting("Android")
+fun Greeting(name: String) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleMedium
+    )
+}
+
+@Composable
+@Preview(group = "my-group-1")
+fun AltGreeting(name: String = "Jetpack Compose") {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleMedium
+    )
+}
+
+class HelloProvider : PreviewParameterProvider<String> {
+    override val values: Sequence<String>
+        get() = listOf("PreviewParameterProvider").asSequence()
+}
+
+@Composable
+@Preview
+fun AltGreeting2(@PreviewParameter(HelloProvider::class)
+                 name: String) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleMedium
+    )
+}
+
+@Composable
+@Preview(locale = "de-rDE")
+fun Welcome() {
+    Text(
+        text = stringResource(id = R.string.welcome),
+        style = MaterialTheme.typography.titleMedium
+    )
+}
+
+@Composable
+fun TextAndButton(name: MutableState<String>, nameEntered: MutableState<Boolean>) {
+    Row(modifier = Modifier.padding(top = 8.dp)) {
+        TextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.hint))
+            },
+            modifier = Modifier
+                .alignByBaseline()
+                .weight(1.0F),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                capitalization = KeyboardCapitalization.Words,
+            ),
+            keyboardActions = KeyboardActions(onAny = {
+                nameEntered.value = true
+            })
+        )
+        Button(modifier = Modifier
+            .alignByBaseline()
+            .padding(8.dp),
+            onClick = {
+                nameEntered.value = true
+            }) {
+            Text(text = stringResource(id = R.string.done))
+        }
+    }
+}
+
+@Composable
+fun Hello() {
+    val name = remember { mutableStateOf("") }
+    val nameEntered = remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (nameEntered.value) {
+            Greeting(name.value)
+        } else {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Welcome()
+                TextAndButton(name, nameEntered)
+            }
+        }
     }
 }
